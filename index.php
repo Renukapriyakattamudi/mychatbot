7d1bbbc86af7816596501f7325727dd6b75acb06
Renukapriyakattamudi <39327936+Renukapriyakattamudi@users.noreply.github.com>
2018-05-29 12:43:31 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,14 +7,10 @@ if($method == 'POST'){
 	$whatvar = $json->result->parameters->whatvar;
 	switch ($whatvar) {
 		case 'Aadhar':
-			$speech = "go to Customer Service > Service Requests > Bank Accounts > 
-			Update your Aadhaar number with Bank account  > Enter the required details and submit it.";
+			$speech = "go to Customer Service > Service Requests > Bank Accounts > Update your Aadhaar number with Bank account  > Enter the required details and submit it.";
 			break;
 		case 'PPF':
-			$speech = "Automatic linking will be done if the Public Provident Fund (PPF) 
-			account is opened under the same Customer ID. 
-			However, if the PPF account is opened with a different User ID, the linking request should be made 
-			at any ICICI Bank branch by submitting the physical form.";
+			$speech = "Automatic linking will be done if the Public Provident Fund (PPF) account is opened under the same Customer ID. However, if the PPF account is opened with a different User ID, the linking request should be made at any ICICI Bank branch by submitting the physical form.";
 			break;
 		case 'moneytransfer':
 			$speech = "Ago to Payments & Transfer > Fund Transfer > Your linked ICICI Bank account / Pockets wallet";
